a4cdadd9ce79a51ffb50a23cd410b46661d98f30
Tcodingg <32871055+Tcodingg@users.noreply.github.com>
2021-04-12 14:11:22 -0400

    add nav bar and style mobile nav

--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import './style.scss'
+import {BiMenu} from 'react-icons/bi'
 
 export default function Main() {
     return (
-        <div>
-            <h1>this is main page</h1>
-        </div>
+        <header className='l-header' id='header'>
+           <nav className='nav bd-container'>
+            <a href="#" className='nav_logo'>Pizza</a>
+            <div className="nav_menu">
+                <ul className="nav_list">
+                    <li className="nav_items"><a href="#home" className='nav_link'>Home</a></li>
+                    <li className="nav_items"><a href="#about" className='nav_link'>About</a></li>
+                    <li className="nav_items"><a href="#services" className='nav_link'>Services</a></li>
+                    <li className="nav_items"><a href="#menu" className='nav_link'>Menu</a></li>
+                    <li className="nav_items"><a href="#contact" className='nav_link'>Contact Us</a></li>
+                    
+                </ul>
+            </div>
+                <div className="nav_toggle" id="nav-toggle">
+                <BiMenu />
+                </div>
+           </nav>
+        </header>
     )
 }
