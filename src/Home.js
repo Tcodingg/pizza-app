0083705cceae70b4ef676d5cd7c5051a1b5039de
Tcodingg <32871055+Tcodingg@users.noreply.github.com>
2021-04-27 14:17:31 -0400

    fix minor issues and add the right pictures

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,20 @@
-import React from 'react'
-import './style.scss'
-import pizza from './images/pizza.png'
+import React from "react";
+import "./style.scss";
+import pizza from "./images/pizza.png";
 
 export default function Home() {
-    return (
-        
-            <section className='home' id='home'>
-                <div className='home_container bd-container bd-grid'>
-                    <div className="home_wrapper">
-                        <h1 className="home_title">Premium Pizza</h1>
-                        <h1 className="home_subtitle">Try Our Best Pizza of the Week.</h1>
-                        <a href="#" className='button'>View Menu</a>
-                    </div>
-                    <img src={pizza} alt="" className="home_img"/>
-                </div>
-               
-            </section>
-      
-    )
+  return (
+    <section className="home" id="home">
+      <div className="home_container bd-container bd-grid">
+        <div className="home_wrapper">
+          <h1 className="home_title">Premium Pizza</h1>
+          <h1 className="home_subtitle">Try Our Best Pizza of the Week.</h1>
+          <a href="#menu" className="button">
+            View Menu
+          </a>
+        </div>
+        <img src={pizza} alt="" className="home_img" />
+      </div>
+    </section>
+  );
 }
--- src/menudata.js
+++ src/menudata.js
@@ -1,30 +1,24 @@
-
-import about_us from './images/about_us.png'
+import about_us from "./images/about_us.png";
+import pizza_2 from "./images/pizza_2.png";
+import pizza_3 from "./images/pizza_3.png";
 
 export const menudata = [
-    {
-        image: about_us,
-        name: 'pepperoni',
-        detail: 'Lorem ipsum dolor sit amet.',
-        price: '$21.99',
-
-
-    },
-    {
-        image:  about_us,
-        name: 'pepperoni',
-        detail: 'Lorem ipsum dolor sit amet.',
-        price: '$24.99',
-
-
-    },
-    {
-        image:  about_us,
-        name: 'pepperoni',
-        detail: 'Lorem ipsum dolor sit amet.',
-        price: '$19.99',
-
-
-    },
-
-]
+  {
+    image: about_us,
+    name: "pepperoni",
+    detail: "Lorem ipsum dolor sit amet.",
+    price: "$21.99",
+  },
+  {
+    image: pizza_2,
+    name: "pepperoni",
+    detail: "Lorem ipsum dolor sit amet.",
+    price: "$24.99",
+  },
+  {
+    image: pizza_3,
+    name: "pepperoni",
+    detail: "Lorem ipsum dolor sit amet.",
+    price: "$19.99",
+  },
+];
