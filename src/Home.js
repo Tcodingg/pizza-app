6ec634702c6c221a4b50a7817b98ecdfdbdbc621
Tcodingg <32871055+Tcodingg@users.noreply.github.com>
2021-05-04 12:13:09 -0400

    change company title from premium pizza to north pizza

--- src/Home.js
+++ src/Home.js
@@ -7,7 +7,7 @@ export default function Home() {
     <section className="home" id="home">
       <div className="home_container bd-container bd-grid">
         <div className="home_wrapper">
-          <h1 className="home_title">Premium Pizza</h1>
+          <h1 className="home_title">North Pizza</h1>
           <h1 className="home_subtitle">Try Our Best Pizza of the Week.</h1>
           <a href="#menu" className="button">
             View Menu
